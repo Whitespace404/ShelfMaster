ae585c4139d64c1861c778deecda23d2a5527fa2
Whitespace404 <rush2asharahul@gmail.com>
2023-08-07 19:41:08 +0530

    add borrow form

--- shelfmaster/__init__.py
+++ shelfmaster/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "28679ae72d9d4c7b0e93b1db218426a6"
 
 from shelfmaster import routes
--- shelfmaster/routes.py
+++ shelfmaster/routes.py
@@ -1,16 +1,27 @@
 from shelfmaster import app
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
+from . import forms
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
-@app.route("/borrow")
+@app.route("/borrow", methods=["GET", "POST"])
 def borrow():
-    return render_template("borrow.html")
+    form = forms.BorrowForm()
+
+    if form.validate_on_submit():
+        flash("Book borrowed successfully.")
+        return redirect(url_for("home"))
+    return render_template("borrow.html", form=form)
 
 
-@app.route("/return")
+@app.route("/return", methods=["GET", "POST"])
 def return_():
     return render_template("return.html")
+
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
