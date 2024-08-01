9729a4a246f172d2f80b7e1970ff8552a6d6ea97
Whitespace404 <rush2asharahul@gmail.com>
2024-08-01 21:19:51 +0530

    xls sheet tweaks, remove greenlet from requirements

--- shelfmaster/utilities_master.py
+++ shelfmaster/utilities_master.py
@@ -60,29 +60,7 @@ def read_booklist():
 
     # return result_dicts
 
-    sheet = wb["4853- 14925"]
-    for row in sheet.iter_rows(min_row=3, values_only=True):
-        details = dict()
-
-        details["accession_number"] = row[0]
-        details["author"] = convert_name(row[1])
-        details["title"] = convert_name(row[2])
-        details["call_number"] = row[3]
-        details["publisher"] = row[4]
-        details["isbn"] = row[5]
-        details["vendor"] = row[6]
-        details["bill_number"] = row[7]
-        details["bill_date"] = row[8]
-        details["price"] = row[9]
-        details["place_of_publication"] = row[10]  # "location"
-        details["remarks"] = row[11]
-        details["language"] = row[12]
-        if any(details.values()):
-            result_dicts.append(details)
-
-    return result_dicts
-
-    # sheet = wb["14926-15874"]
+    # sheet = wb["4853- 14925"]
     # for row in sheet.iter_rows(min_row=3, values_only=True):
     #     details = dict()
 
@@ -103,6 +81,28 @@ def read_booklist():
     #         result_dicts.append(details)
 
     # return result_dicts
+
+    sheet = wb["14926-15874"]
+    for row in sheet.iter_rows(min_row=3, values_only=True):
+        details = dict()
+
+        details["accession_number"] = row[0]
+        details["author"] = convert_name(row[1])
+        details["title"] = convert_name(row[2])
+        details["call_number"] = row[3]
+        details["publisher"] = row[4]
+        details["isbn"] = row[5]
+        details["vendor"] = row[6]
+        details["bill_number"] = row[7]
+        details["bill_date"] = row[8]
+        details["price"] = row[9]
+        details["place_of_publication"] = row[10]  # "location"
+        details["remarks"] = row[11]
+        details["language"] = row[12]
+        if any(details.values()):
+            result_dicts.append(details)
+
+    return result_dicts
 
 
 def read_namelist():
